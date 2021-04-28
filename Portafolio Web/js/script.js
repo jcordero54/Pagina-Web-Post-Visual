6628cb138e982b30f399a73bfbaa7865654c9cdd
jcordero54 <74500756+jcordero54@users.noreply.github.com>
2021-04-28 09:05:13 -0500

    Add files via upload

---
+++ Portafolio Web/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function)(){
+
+    /* Activación de Slide */
+    $('.slide-a').slick({
+        autoplay: true,
+        slideToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 2000,
+        speed: 250,
+        infinite: true,
+        fade: true
+    })
+
+});
